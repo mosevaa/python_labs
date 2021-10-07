651947906fccb7db31ea1d67b6cc36dfaa9b3806
Alesya Moseva <mosevaa13@gmail.com>
2021-10-07 23:51:24 +0300

    lab3

---
+++ lab3/blog/articles/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+
+# Create your views here.
+from .models import Article
+from django.shortcuts import render
+
+def archive(request):
+    return render(request, 'archive.html', {"posts": Article.objects.all()})
+
